5452ea4d2ecadf6ed1c524c22756e3fc892f21f7
Christina Lu <christina.lu.12@gmail.com>
2022-04-24 19:02:53 -0400

    Fix login/signup buttons and style login screen

--- anxietea/views.py
+++ anxietea/views.py
@@ -7,30 +7,28 @@ from django.contrib.auth import authenticate, login, logout
 def onboarding(request):
     return render(request, 'anxietea/onboarding.html')
 
-def splash_view(request):
-     return render(request, 'anxietea/splash.html' ) 
-
 def emotions_view(request):
     return render(request, 'anxietea/emotions.html' )
 
-
 def login_view(request):
-    username, password = request.POST['username'], request.POST['password']
-    user = authenticate(username=username, password=password)
-    if user is not None:
-        login(request, user)
-        return redirect('/')
-    else:
-        return redirect('/splash?error=LoginError')
+    # username, password = request.POST['username'], request.POST['password']
+    # user = authenticate(username=username, password=password)
+    # if user is not None:
+    #     login(request, user)
+    #     return redirect('/')
+    # else:
+    #     return redirect('/splash?error=LoginError')
+    return render(request, 'anxietea/login.html' )
 
 def signup_view(request):
-    user = User.objects.create_user(
-        username=request.POST['username'],
-        password=request.POST['password'],
-        email=request.POST['email'],
-    )
-    login(request, user)
-    return redirect('/')
+    # user = User.objects.create_user(
+    #     username=request.POST['username'],
+    #     password=request.POST['password'],
+    #     email=request.POST['email'],
+    # )
+    # login(request, user)
+    # return redirect('/')
+    return render(request, 'anxietea/signup.html')
 
 def logout_view(request):
     logout(request)
